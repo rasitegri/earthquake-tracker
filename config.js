fd11ebdeed04b57cd46f66f590b4f07a14a4dde4
rasit-vultr <egri.rasid@gmail.com>
2024-04-28 20:07:17 +0000

    somehow mongo doesn't like localhost. change w/ 127.0.0.1

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const password = process.env.MONGODB_EARTHQUAKE_PASSWORD;
 const authDb = process.env.MONGODB_EARTHQUAKE_AUTH_DB;
 const dbName = authDb;
 
-const databaseUrl = `mongodb://${user}:${password}@localhost:27017/${dbName}?authSource=${authDb}`;
+const databaseUrl = `mongodb://${user}:${password}@127.0.0.1:27017/${dbName}?authSource=${authDb}`;
 
 module.exports = {
     databaseUrl
